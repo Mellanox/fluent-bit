572ce33ecf293c476bf8244219b9a32ef9468144
Roman Pryamonosov <romanpr@nvidia.com>
2022-04-05 19:03:51 +0300

    shared library and input plugin for raw MessagePack data input

--- cmake/plugins_options.cmake
+++ cmake/plugins_options.cmake
@@ -63,6 +63,8 @@ DEFINE_OPTION(FLB_IN_WINDOWS_EXPORTER_METRICS "Enable windows exporter metrics i
 DEFINE_OPTION(FLB_IN_WINEVTLOG                "Enable Windows EvtLog input plugin"           OFF)
 DEFINE_OPTION(FLB_IN_WINSTAT                  "Enable Windows Stat input plugin"             OFF)
 DEFINE_OPTION(FLB_IN_EBPF                     "Enable Linux eBPF input plugin"               OFF)
+DEFINE_OPTION(FLB_IN_RAW_MSGPACK              "Enable Raw MessagePack input plugin"          ON)
+
 
 # Processors
 # ==========
